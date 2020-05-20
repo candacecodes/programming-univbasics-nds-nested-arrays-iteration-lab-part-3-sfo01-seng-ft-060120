4cc561e4645350fde998ff54d7314ffa0e78a7c2
eager-class-5814 <candacehchoe@gmail.com>
2020-05-20 02:11:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ while row_index < src.count do
   element_index = 0 
   while element_index < src[row_index].count do 
     if element_index = String 
-      phrase = phrase + {element_index} 
+      phrase = phrase + `${element_index}` 
     else #not string? 
       phrase += ""
     end 
